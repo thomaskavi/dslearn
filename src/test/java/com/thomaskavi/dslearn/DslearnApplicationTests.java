087711d1773c3f837454099547d39de6d2aabfd1
thomaskavi <thomaskavi2000@gmail.com>
2025-01-06 20:08:41 -0300

    first commit

---
+++ src/test/java/com/thomaskavi/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomaskavi.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
