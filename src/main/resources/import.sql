dfbbeebabea6c3024e7c773cc1064b04739c010c
thomaskavi <thomaskavi2000@gmail.com>
2025-01-06 20:14:07 -0300

    Course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,4 +15,4 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
