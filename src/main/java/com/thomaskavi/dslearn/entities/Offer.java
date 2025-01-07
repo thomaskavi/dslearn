881120c004b935f95d89ac59477432e1978a1eed
thomaskavi <thomaskavi2000@gmail.com>
2025-01-07 13:37:46 -0300

    Fix: Offer timestamp without timezone

--- src/main/java/com/thomaskavi/dslearn/entities/Offer.java
+++ src/main/java/com/thomaskavi/dslearn/entities/Offer.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,7 +22,11 @@ public class Offer {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String edition;
+
+  @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant startMoment;
+
+  @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant endMoment;
 
   @ManyToOne
