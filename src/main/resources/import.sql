08513356bfe34f57a00b972dacdd1cf72c5bb2b8
thomaskavi <thomaskavi2000@gmail.com>
2025-01-07 13:13:59 -0300

    Section

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -21,7 +21,9 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2025-12-01T03:00:00Z', TIMESTAMP WITH TIME ZONE '2026-12-01T03:00:00Z', 1);
 
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 1, 1)
-
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 2, 1)
-
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 0, 1)
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 1, 2);
