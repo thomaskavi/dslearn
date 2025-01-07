c296636222e1ff26bd37f28136ed96e5f58ab2cb
thomaskavi <thomaskavi2000@gmail.com>
2025-01-07 13:02:34 -0300

    Resource

---
+++ src/main/java/com/thomaskavi/dslearn/entities/Resource.java
@@ -0,0 +1,125 @@
+package com.thomaskavi.dslearn.entities;
+
+import com.thomaskavi.dslearn.entities.enums.ResourceType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_resource")
+public class Resource {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private String description;
+  private Integer position;
+  private String imgUri;
+  private ResourceType type;
+
+  @ManyToOne
+  @JoinColumn(name = "offer_id")
+  private Offer offer;
+
+  public Resource() {
+  }
+
+  public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type,
+      Offer offer) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.position = position;
+    this.imgUri = imgUri;
+    this.type = type;
+    this.offer = offer;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Integer getPosition() {
+    return position;
+  }
+
+  public void setPosition(Integer position) {
+    this.position = position;
+  }
+
+  public String getImgUri() {
+    return imgUri;
+  }
+
+  public void setImgUri(String imgUri) {
+    this.imgUri = imgUri;
+  }
+
+  public ResourceType getType() {
+    return type;
+  }
+
+  public void setType(ResourceType type) {
+    this.type = type;
+  }
+
+  public Offer getOffer() {
+    return offer;
+  }
+
+  public void setOffer(Offer offer) {
+    this.offer = offer;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Resource other = (Resource) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,3 +19,9 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2025-01-01T03:00:00Z', TIMESTAMP WITH TIME ZONE '2026-01-01T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2025-12-01T03:00:00Z', TIMESTAMP WITH TIME ZONE '2026-12-01T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 1, 1)
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 2, 1)
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 0, 1)
