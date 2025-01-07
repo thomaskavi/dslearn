c3804577d97905ae3888fb7e9b5e635116b15914
thomaskavi <thomaskavi2000@gmail.com>
2025-01-07 13:36:15 -0300

    Enrollment seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -27,3 +27,6 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://www.seobility.net/en/wiki/images/a/a6/HTML-Doctype.png', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2025-01-01T13:00:00Z', null, true, false)
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2025-01-01T13:00:00Z', null, true, false)
